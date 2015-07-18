9362c6ec77856a39090588319b7e10bdb815b3d5
Ken Wilcox <wilcoxk@gmail.com>
2015-07-18 00:22:47 -0600

    Simple UI

--- Hello_Mac/MainWindow.designer.cs
+++ Hello_Mac/MainWindow.designer.cs
@@ -1,17 +1,38 @@
-﻿
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoMac.Foundation;
+using System.CodeDom.Compiler;
+
 namespace Hello_Mac
 {
-  
-  // Should subclass MonoMac.AppKit.NSWindow
-  [MonoMac.Foundation.Register("MainWindow")]
-  public partial class MainWindow
-  {
-  }
-  
-  // Should subclass MonoMac.AppKit.NSWindowController
-  [MonoMac.Foundation.Register("MainWindowController")]
-  public partial class MainWindowController
-  {
-  }
-}
+	[Register ("MainWindowController")]
+	partial class MainWindowController
+	{
+		[Outlet]
+		MonoMac.AppKit.NSTextField ClickedLabel { get; set; }
 
+		[Action ("ClickedButton:")]
+		partial void ClickedButton (MonoMac.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (ClickedLabel != null) {
+				ClickedLabel.Dispose ();
+				ClickedLabel = null;
+			}
+		}
+	}
+
+	[Register ("MainWindow")]
+	partial class MainWindow
+	{
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
